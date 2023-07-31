8d4ebfdd3935a680056dce9ce06f372b735f5dad
emilija <emilija1705@gmail.com>
2023-07-31 16:20:43 +0200

    code maintenance

--- tapas/src/main.py
+++ tapas/src/main.py
@@ -4,19 +4,25 @@ from io_func import *
 
 def main():
 
-    architecture, ecu, bus = file_select()
+    #architecture, ecu, bus = file_select()
+
+    architecture = "C:\\Dev\\bachelor-thesis\\tapas\\graphspoc\\a.json"
+    ecu = "C:\\Dev\\bachelor-thesis\\tapas\\ecuspoc\\ecusA.json"
+    bus = "C:\\Dev\\bachelor-thesis\\tapas\\buses\\buses.json"
 
     print(architecture)
 
     architecture_name, architecture, ecus_config, buses_config = parse_files(architecture, ecu, bus)
 
-    G, entry_ecus, target_ecus_names = generate_graph(architecture, ecus_config, buses_config)
+    G, entry_ecus, target_ecus = generate_graph(architecture, ecus_config, buses_config)
 
-    table = find_attack_path(G, entry_ecus, target_ecus_names)
+    table = find_attack_path(G, entry_ecus, target_ecus)
 
     print_table(table, architecture_name)
 
-    architecture = apply_criteria(entry_ecus, target_ecus_names, table, architecture)
+    total_architecture_feasibility = apply_criteria(entry_ecus, target_ecus, table, architecture)
+
+    export_to_excel(table, architecture_name, total_architecture_feasibility)
 
 
 if __name__ == "__main__":
